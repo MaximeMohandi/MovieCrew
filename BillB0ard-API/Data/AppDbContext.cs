4541690dac498abcbcf4e9110ddcb87fde9c89f2
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-12-10 10:16:05 +0100

    add db context

---
+++ BillB0ard-API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BillB0ard_API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillB0ard_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
