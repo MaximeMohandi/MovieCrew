2784321ac37127446501b698781af91d4ab61752
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-12-10 10:25:35 +0100

    add InMemoryStructure for database test

--- BillB0ard-API.Test/FetchMoviesFromDatabase.cs
+++ BillB0ard-API.Test/FetchMoviesFromDatabase.cs
@@ -1,12 +1,29 @@
+using BillB0ard_API.Data;
 using BillB0ard_API.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BillB0ard_API.Test
 {
     public class Tests
     {
-        [SetUp]
+        private static DbContextOptions<AppDbContext> _dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "MovieDbTest")
+            .Options;
+
+        AppDbContext dbContext;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            dbContext = new AppDbContext(_dbContextOptions);
+            dbContext.Database.EnsureCreated();
+
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            dbContext.Database.EnsureDeleted();
         }
 
         [Test]
