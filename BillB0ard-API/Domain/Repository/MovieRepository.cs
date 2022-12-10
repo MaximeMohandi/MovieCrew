961795a830666ef003008082d492a663a4673228
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-12-10 11:18:08 +0100

    clean space

--- BillB0ard-API/Domain/Repository/MovieRepository.cs
+++ BillB0ard-API/Domain/Repository/MovieRepository.cs
@@ -14,12 +14,18 @@ namespace BillB0ard_API.Domain.Repository
 
         public async Task<Movie> GetMovie(string title)
         {
-            return await _dbContext.Movies.Where(m => m.Name.ToLower() == title.ToLower()).Select(m => new Movie(m.Id, m.Name, m.Poster, m.DateAdded, m.SeenDate)).FirstOrDefaultAsync();
+            return await _dbContext.Movies
+                    .Where(m => m.Name.ToLower() == title.ToLower())
+                    .Select(m => new Movie(m.Id, m.Name, m.Poster, m.DateAdded, m.SeenDate))
+                    .FirstOrDefaultAsync();
         }
 
         public async Task<Movie> GetMovie(int id)
         {
-            return await _dbContext.Movies.Where(m => m.Id == id).Select(m => new Movie(m.Id, m.Name, m.Poster, m.DateAdded, m.SeenDate)).FirstOrDefaultAsync();
+            return await _dbContext.Movies
+                .Where(m => m.Id == id)
+                .Select(m => new Movie(m.Id, m.Name, m.Poster, m.DateAdded, m.SeenDate))
+                .FirstOrDefaultAsync();
         }
     }
 }
