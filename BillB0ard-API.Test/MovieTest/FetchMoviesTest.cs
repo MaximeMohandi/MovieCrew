fa2804ebe898573f2b410aebc38f3bbd21c035c2
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-12-11 14:50:54 +0100

    clean space

--- BillB0ard-API.Test/MovieTest/FetchMoviesTest.cs
+++ BillB0ard-API.Test/MovieTest/FetchMoviesTest.cs
@@ -127,9 +127,6 @@ namespace BillB0ard_API.Test.Movies
             Assert.That(ex.Message, Is.EqualTo("There's no movie with the id : -1. Please check the given id and retry."));
         }
 
-
-
-
         private void SeedInMemoryDatas()
         {
             Movie[] movies = new[]
