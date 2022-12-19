7a1c004728a5921003aa2acd0cf3ce69efad8b38
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-12-19 15:19:22 +0100

    try to conform to the recommended serialization pattern.

--- BillB0ard-API/Domain/Exception/RateException.cs
+++ BillB0ard-API/Domain/Exception/RateException.cs
@@ -1,11 +1,18 @@
-﻿namespace BillB0ard_API.Domain.Exception
+﻿using System.Runtime.Serialization;
+
+namespace BillB0ard_API.Domain.Exception
 {
     [Serializable]
-    public class RateException : System.Exception
+    public class RateException : System.Exception, ISerializable
     {
         public RateException(string message) : base(message) { }
+
+        protected RateException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext)
+        {
+        }
     }
 
+    [Serializable]
     public class RateLimitException : RateException
     {
         public RateLimitException(decimal rate) : base($"The rate must be between 0 and 10. Actual : {rate}") { }
