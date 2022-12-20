ab061e4e6ec900ee20350676b8aeb3e865de7bca
Maxime Mohandi <mohandi.maxime@gmail.com>
2022-12-20 10:50:56 +0100

    add some coverage for rate equality

--- BillB0ard-API.Test/RateTest/RateEqualityTest.cs
+++ BillB0ard-API.Test/RateTest/RateEqualityTest.cs
@@ -29,5 +29,39 @@ namespace BillB0ard_API.Test.RateTest
 
             Assert.That(actualRate, Is.EqualTo(expectedRate));
         }
+
+        [Test]
+        public void RateCompareToNull()
+        {
+            var actualRate = new Rate
+            {
+                Movie = new Movie(),
+                MovieId = 1,
+                Note = 2.0M,
+                User = new User(),
+                UserId = 1,
+
+            };
+
+            Assert.That(actualRate, Is.Not.EqualTo(null));
+        }
+
+        [Test]
+        public void NotEqualToOtherType()
+        {
+            Rate actualRate = new()
+            {
+                Movie = new Movie(),
+                MovieId = 1,
+                Note = 2.0M,
+                User = new User(),
+                UserId = 1,
+
+            };
+
+            object otherObject = new() { };
+
+            Assert.That(actualRate, Is.Not.EqualTo(otherObject));
+        }
     }
 }
