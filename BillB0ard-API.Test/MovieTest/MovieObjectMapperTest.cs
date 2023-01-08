8b179ef973311b0bf4fdf805aac19c16cdc28bb3
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-01-08 20:39:22 +0100

    model to entity extesntion

---
+++ BillB0ard-API.Test/MovieTest/MovieObjectMapperTest.cs
@@ -0,0 +1,27 @@
+﻿using BillB0ard_API.Data.Models;
+using BillB0ard_API.Domain.Entities;
+using BillB0ard_API.Domain.Mappers;
+
+namespace BillB0ard_API.Test.MovieTest
+{
+    public class MovieObjectMapperTest
+    {
+        [Test]
+        public void MapModelWithoutRateToEntity()
+        {
+            Movie movieModel = new Movie
+            {
+                Id = 8,
+                Name = "Lamar Jackson",
+                DateAdded = new(2023, 1, 8),
+                Poster = "http://MVP2019.NFL",
+                SeenDate = new(2023, 1, 8)
+            };
+            MovieEntity expectedEntity = new(8, "Lamar Jackson", "http://MVP2019.NFL", new(2023, 1, 8), new(2023, 1, 8));
+
+            MovieEntity entity = movieModel.ToEntity();
+
+            Assert.That(entity, Is.EqualTo(expectedEntity));
+        }
+    }
+}
---
+++ BillB0ard-API/Domain/Mappers/MovieMapperExtension.cs
@@ -0,0 +1,13 @@
+﻿using BillB0ard_API.Data.Models;
+using BillB0ard_API.Domain.Entities;
+
+namespace BillB0ard_API.Domain.Mappers
+{
+    public static class MovieMapperExtension
+    {
+        public static MovieEntity ToEntity(this Movie model)
+        {
+            return new(model.Id, model.Name, model.Poster, model.DateAdded, model.SeenDate);
+        }
+    }
+}
