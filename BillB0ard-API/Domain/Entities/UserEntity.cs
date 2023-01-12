67ce0b443c4486eeca4fcc15773027b00e62f90c
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-01-12 13:53:47 +0100

    <fix> equality of user entity based on reference instead of value

--- BillB0ard-API/Domain/Entities/UserEntity.cs
+++ BillB0ard-API/Domain/Entities/UserEntity.cs
@@ -14,5 +14,23 @@ namespace BillB0ard_API.Domain.Entities
         public long Id { get; }
         public string Name { get; }
         public UserRoles Role { get; }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            UserEntity toCompare = (UserEntity)obj;
+            return Id.Equals(toCompare.Id) && Name.Equals(toCompare.Name)
+                && Role.Equals(toCompare.Role);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ Name.GetHashCode();
+        }
     }
 }
