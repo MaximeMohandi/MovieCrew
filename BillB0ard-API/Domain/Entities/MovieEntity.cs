04fae1ad8c1ae62ab39dcb0dfbb0b8be2394f5ec
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-01-31 19:42:46 +0100

    rename properties for more clarity

--- BillB0ard-API/Domain/Entities/MovieEntity.cs
+++ BillB0ard-API/Domain/Entities/MovieEntity.cs
@@ -2,13 +2,13 @@
 {
     public class MovieEntity
     {
-        public MovieEntity(int id, string title, string? poster, DateTime addedDate, DateTime? seenDate)
+        public MovieEntity(int id, string title, string? poster, DateTime dateAdded, DateTime? viewingDate)
         {
             this.Id = id;
             this.Title = title;
             this.Poster = poster;
-            this.AddedDate = addedDate;
-            this.SeenDate = seenDate;
+            this.DateAdded = dateAdded;
+            this.ViewingDate = viewingDate;
         }
 
         public MovieEntity(int id, string title, string? poster, DateTime addedDate, DateTime? seenDate, decimal? averageRate)
@@ -16,16 +16,16 @@
             this.Id = id;
             this.Title = title;
             this.Poster = poster;
-            this.AddedDate = addedDate;
-            this.SeenDate = seenDate;
+            this.DateAdded = addedDate;
+            this.ViewingDate = seenDate;
             this.AverageRate = averageRate;
         }
 
         public int Id { get; }
         public string Title { get; }
         public string? Poster { get; }
-        public DateTime AddedDate { get; }
-        public DateTime? SeenDate { get; }
+        public DateTime DateAdded { get; }
+        public DateTime? ViewingDate { get; }
         public decimal? AverageRate { get; } = null;
 
         public override bool Equals(object? obj)
@@ -36,19 +36,19 @@
 
 
             return Id.Equals(toCompare.Id) && Title == toCompare.Title
-                && Poster == toCompare.Poster && AddedDate == toCompare.AddedDate
-                && SeenDate == toCompare.SeenDate && AverageRate == toCompare.AverageRate;
+                && Poster == toCompare.Poster && DateAdded == toCompare.DateAdded
+                && ViewingDate == toCompare.ViewingDate && AverageRate == toCompare.AverageRate;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Id, Title, AddedDate) + HashcodesNullablePropoerties();
+            return HashCode.Combine(Id, Title, DateAdded) + HashcodesNullablePropoerties();
         }
 
         private int HashcodesNullablePropoerties()
         {
             return (Poster is null ? 0 : Poster.GetHashCode())
-                + (SeenDate is null ? 0 : SeenDate.GetHashCode())
+                + (ViewingDate is null ? 0 : ViewingDate.GetHashCode())
                 + (AverageRate is null ? 0 : AverageRate.GetHashCode());
         }
     }
