a1efdb2f52c1fd458e10bab080af6ac71f23a78a
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-02-02 11:46:47 +0100

    remove serialization on custom exception because it's not used

--- BillB0ard-API/Domain/Exception/RateException.cs
+++ BillB0ard-API/Domain/Exception/RateException.cs
@@ -1,26 +1,12 @@
-﻿using System.Runtime.Serialization;
-
-namespace BillB0ard_API.Domain.Exception
+﻿namespace BillB0ard_API.Domain.Exception
 {
-    [Serializable]
     public class RateException : System.Exception
     {
         public RateException(string message) : base(message) { }
-
-        protected RateException(SerializationInfo serializationInfo, StreamingContext streamingContext)
-            : base(serializationInfo, streamingContext)
-        {
-        }
     }
 
-    [Serializable]
     public class RateLimitException : RateException
     {
         public RateLimitException(decimal rate) : base($"The rate must be between 0 and 10. Actual : {rate}") { }
-
-        protected RateLimitException(SerializationInfo serializationInfo, StreamingContext streamingContext)
-            : base(serializationInfo, streamingContext)
-        {
-        }
     }
 }
