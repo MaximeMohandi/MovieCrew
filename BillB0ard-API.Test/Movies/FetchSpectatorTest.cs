b62e6dcdc8d210759e20cae3c874a4e57f296e94
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-02-08 10:58:31 +0100

    add spectator object

---
+++ BillB0ard-API.Test/Movies/FetchSpectatorTest.cs
@@ -0,0 +1,21 @@
+﻿using BillB0ard_API.Domain.Movies.Entities;
+
+namespace BillB0ard_API.Test.Movies
+{
+    public class FetchSpectatorTest
+    {
+        [Test]
+        public void OneSpectatorWithAllItsMovies()
+        {
+            var actual = new SpectatorEntity(new(1, "Manon"), new()
+            {
+                new(new MovieEntity(1, "Babylon", null, new(2023, 2, 8), new(2023, 2, 9), 3M), 3M)
+            });
+            var expected = new SpectatorEntity(new(1, "Manon"), new()
+            {
+                new(new MovieEntity(1, "Babylon", null, new(2023, 2, 8), new(2023, 2, 9), 3M), 3M)
+            });
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ BillB0ard-API/Domain/Movies/Entities/SpectatorRateEntity.cs
@@ -0,0 +1,4 @@
+﻿namespace BillB0ard_API.Domain.Movies.Entities
+{
+    public record SpectatorRateEntity(MovieEntity RatedMovie, decimal Rate);
+}
