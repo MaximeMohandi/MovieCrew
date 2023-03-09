9766a1a66111a9d52431851962cd9227c138f91e
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-03-09 19:11:12 +0100

    connect to api

--- MovieCrew_core.Test/Movies/ThirdPartyMovieDataTest.cs
+++ MovieCrew_core.Test/Movies/ThirdPartyMovieDataTest.cs
@@ -1,20 +1,29 @@
-﻿using MovieCrew.Core.Domain.Movies.Services;
+﻿using Microsoft.Extensions.Configuration;
+using MovieCrew.Core.Domain.Movies.Services;
 
 namespace BillB0ard_API.Test.Movies
 {
     public class ThirdPartyMovieDataTest
     {
+        private string _apiKey;
+        private string _apiUrl;
 
         [SetUp]
         public void SetUp()
         {
-            //todo get user secret 
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<ThirdPartyMovieDataTest>()
+                .Build();
+
+            _apiKey = configuration["ThirdPartyMovieData:apiKey"];
+            _apiUrl = configuration["ThirdPartyMovieData:url"];
+
         }
 
         [Test]
         public async Task ICanConnectToThirdParty()
         {
-            var thirdPartyProvider = new ThirdPartyMovieDataProvider("", "");
+            var thirdPartyProvider = new ThirdPartyMovieDataProvider(_apiUrl, _apiKey);
 
             bool actual = await thirdPartyProvider.GetDetails();
 
