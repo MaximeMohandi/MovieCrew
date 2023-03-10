d4df2f9a9b5dafa840003ddcc67c3f0b4f1858f6
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-03-10 22:06:58 +0100

    fetching metadata work

--- MovieCrew_core/Domain/Movies/Entities/MovieMetadataEntity.cs
+++ MovieCrew_core/Domain/Movies/Entities/MovieMetadataEntity.cs
@@ -4,6 +4,7 @@ namespace MovieCrew.Core.Domain.Movies.Entities
 {
     public class MovieMetadataEntity
     {
+        private string _posterLink;
         public MovieMetadataEntity(string posterLink, string description, decimal ratings, decimal revenue)
         {
             PosterLink = posterLink;
@@ -12,7 +13,7 @@ namespace MovieCrew.Core.Domain.Movies.Entities
             Revenue = revenue;
         }
         [JsonPropertyName("poster_path")]
-        public string PosterLink { get; }
+        public string PosterLink { get => _posterLink; set => _posterLink = "https://image.tmdb.org/t/p/original" + value; }
 
         [JsonPropertyName("overview")]
         public string Description { get; }
