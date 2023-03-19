628d0ce86b5a8fc706c0360dbcda61b015677983
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-03-19 18:15:26 +0100

    wip : add controller

---
+++ MovieCrew.API.Test/Controller/Ratings/RateController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieCrew.API.Test.Controller.Ratings
+{
+    [Route("api/rate")]
+    [ApiController]
+    public class RateController
+    {
+        [HttpPost("add")]
+        public async Task<ActionResult<string>> RateMovie(int v1, int v2, decimal v3)
+        {
+            return Ok("");
+        }
+    }
+}
--- MovieCrew.API.Test/Controller/Ratings/RateMovie.cs
+++ MovieCrew.API.Test/Controller/Ratings/RateMovie.cs
@@ -1,7 +1,18 @@
-﻿namespace MovieCrew.API.Test.Controller.Ratings
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieCrew.API.Test.Controller.Ratings
 {
     public class RateMovie
     {
+        [Test]
+        public async Task RateMovie()
+        {
+            RateController controller = new RateController();
+            var actual = (await controller.RateMovie(1, 2, 3.0M)).Result as ObjectResult;
 
+            Assert.That(actual.StatusCode, Is.EqualTo(StatusCodes.Status201Created));
+
+        }
     }
 }
