f57067e4051a40d3f8c621ba79f3919fbf2d93d9
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-05-12 22:56:31 +0200

    refactor: splited all and details movie endpoints integration test

--- MovieCrew.API.Test/Integration/Movie/DetailsMovieEndpointTest.cs
+++ MovieCrew.API.Test/Integration/Movie/DetailsMovieEndpointTest.cs
@@ -1,78 +1,15 @@
-﻿using System.Text.Encodings.Web;
-using System.Text.Json;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Moq;
 using MovieCrew.Core.Domain.Movies.Entities;
 using MovieCrew.Core.Domain.Movies.Exception;
-using MovieCrew.Core.Domain.Movies.Services;
 using MovieCrew.Core.Domain.Users.Entities;
 using MovieCrew.Core.Domain.Users.Enums;
 
 namespace MovieCrew.API.Test.Integration.Movie;
 
-public class MovieRoutesTest
+public class DetailsMovieEndpointTest : MovieEndpointTestBase
 {
-    private readonly JsonSerializerOptions _jsonOptions;
-    private HttpClient _client;
-    private Mock<IMovieService> _movieService;
-
-    public MovieRoutesTest()
-    {
-        _jsonOptions = new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-        };
-    }
-
-    [SetUp]
-    public void SetUp()
-    {
-        _movieService = new Mock<IMovieService>();
-        _client = new IntegrationTestServer<IMovieService>(_movieService).CreateDefaultClient();
-    }
-
-    [Test]
-    public async Task FetchAllMovie()
-    {
-        var expectedList = new List<MovieEntity>
-        {
-            new(1, "Tempête", "http://url", "lorem Ipsum", new DateTime(2023, 3, 11), new DateTime(2023, 3, 18),
-                2),
-            new(1, "John Wick", "http://url", "lorem Ipsum", new DateTime(2023, 3, 11),
-                new DateTime(2023, 3, 18),
-                5.25M)
-        };
-        _movieService.Setup(x => x.FetchAllMovies())
-            .ReturnsAsync(expectedList);
-
-        var expectedJsonResponse = JsonSerializer.Serialize(expectedList, _jsonOptions);
-
-        var response = await _client.GetAsync("/api/movie/all");
-        var responseContent = await response.Content.ReadAsStringAsync();
-
-        Assert.Multiple(() =>
-        {
-            Assert.That((int)response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
-            Assert.That(responseContent.ToLower(), Is.EqualTo(expectedJsonResponse.ToLower()));
-        });
-    }
-
-    [Test]
-    public async Task NoMoviesToFetch()
-    {
-        var expected = "it seems that there's no movies in the list. please try to add new one";
-        _movieService.Setup(x => x.FetchAllMovies()).ThrowsAsync(new NoMoviesFoundException());
-
-        var response = await _client.GetAsync("/api/movie/all");
-        var responseContent = await response.Content.ReadAsStringAsync();
-
-        Assert.Multiple(() =>
-        {
-            Assert.That((int)response.StatusCode, Is.EqualTo(StatusCodes.Status404NotFound));
-            Assert.That(responseContent.ToLower(), Is.EqualTo(expected.ToLower()));
-        });
-    }
-
     [Test]
     public async Task FetchMoviesDetailWithId()
     {
@@ -132,7 +69,7 @@ public class MovieRoutesTest
         Assert.Multiple(() =>
         {
             Assert.That((int)response.StatusCode, Is.EqualTo(StatusCodes.Status404NotFound));
-            Assert.That(responseContent.ToLower(),
+            Assert.That(responseContent,
                 Is.EqualTo("There's no movie with the id : 1. Please check the given id and retry."));
         });
     }
