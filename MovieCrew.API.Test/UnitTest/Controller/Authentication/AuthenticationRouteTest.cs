3ee227c09ff988ee79fafdc9598ab32783ea22f2
Maxime Mohandi <mohandi.maxime@gmail.com>
2023-05-13 11:38:54 +0200

    refactor(class): rearrange authentication controller unit test

--- MovieCrew.API.Test/UnitTest/Controller/Authentication/AuthenticationRouteTest.cs
+++ MovieCrew.API.Test/UnitTest/Controller/Authentication/AuthenticationRouteTest.cs
@@ -11,27 +11,31 @@ namespace MovieCrew.API.Test.UnitTest.Controller.Authentication;
 public class AuthenticationRouteTest
 {
     private const string IsCorrectToken = @"^([a-zA-Z0-9_-]+\.){2}[a-zA-Z0-9_-]+$";
+    private AuthenticationController _authenticationController;
     private Mock<IAuthenticationRepository> _authRepositoryMock;
 
     [SetUp]
     public void Setup()
     {
         _authRepositoryMock = new Mock<IAuthenticationRepository>();
+        var jwtConfiguration =
+            new JwtConfiguration("A.ComplexP4ss3@Phrase", "http://test.issuer", "http://test.audience");
+        var authenticationService = new AuthenticationService(_authRepositoryMock.Object, jwtConfiguration);
+        _authenticationController = new AuthenticationController(authenticationService);
     }
 
     [Test]
-    public async Task Login()
+    public async Task SuccessLoginShouldReturnAuthenticatedUser()
     {
+        // Arrange
         _authRepositoryMock.Setup(x => x.IsUserExist(1, "test"))
             .ReturnsAsync(true);
-        JwtConfiguration jwtConfiguration =
-            new("A.ComplexP4ss3@Phrase", "http://test.issuer", "http://test.audience");
-        AuthenticationService authenticationService = new(_authRepositoryMock.Object, jwtConfiguration);
-        AuthenticationController authenticationController = new(authenticationService);
 
-        var actual = (await authenticationController.Get(new UserLoginDto(1, "test"))).Result as ObjectResult;
+        // Act
+        var actual = (await _authenticationController.Get(new UserLoginDto(1, "test"))).Result as ObjectResult;
         var actualAuthenticatedUser = actual.Value as AuthenticatedUser;
 
+        // Assert
         Assert.Multiple(() =>
         {
             Assert.That(actualAuthenticatedUser.UserName, Is.EqualTo("test"));
@@ -44,16 +48,12 @@ public class AuthenticationRouteTest
     }
 
     [Test]
-    public async Task LoginFailInvalidUserReturn403ForbiddenError()
+    public async Task ShouldReturnForbiddenWhenInvalidUser()
     {
-        _authRepositoryMock.Setup(x => x.IsUserExist(1, "test"))
-            .ReturnsAsync(true);
-        JwtConfiguration jwtConfiguration =
-            new("A.ComplexP4ss3@Phrase", "http://test.issuer", "http://test.audience");
-        AuthenticationService authenticationService = new(_authRepositoryMock.Object, jwtConfiguration);
-        AuthenticationController authenticationController = new(authenticationService);
+        _authRepositoryMock.Setup(x => x.IsUserExist(2, "test"))
+            .ReturnsAsync(false);
 
-        var actual = (await authenticationController.Get(new UserLoginDto(2, "test"))).Result as ObjectResult;
+        var actual = (await _authenticationController.Get(new UserLoginDto(2, "test"))).Result as ObjectResult;
 
         Assert.That(actual.StatusCode, Is.EqualTo(StatusCodes.Status403Forbidden));
     }
